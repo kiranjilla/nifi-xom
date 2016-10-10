a6bb5d75be851eb3ae993bd5eeefb7630310fc4c
Sekhar Babu <sbabu@hortonworks.com>
2016-10-09 17:57:04 -0700

    opcda: Create nifi-opcda-bundle base version

---
+++ nifi-nar-bundles/nifi-opcda-bundle/nifi-opcdaclient-processors/src/main/java/org/apache/nifi/processors/opcdaclient/util/OPCInitialTagConfig.java
@@ -0,0 +1,193 @@
+package org.apache.nifi.processors.opcdaclient.util;
+
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
+import java.util.TreeMap;
+
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.logging.ComponentLog;
+import org.jinterop.dcom.common.JIException;
+import org.openscada.opc.lib.common.NotConnectedException;
+import org.openscada.opc.lib.da.AddFailedException;
+import org.openscada.opc.lib.da.DuplicateGroupException;
+import org.openscada.opc.lib.da.Group;
+import org.openscada.opc.lib.da.Item;
+import org.openscada.opc.lib.da.ItemState;
+import org.openscada.opc.lib.da.Server;
+import org.openscada.opc.lib.da.UnknownGroupException;
+import org.openscada.opc.lib.da.browser.Branch;
+import org.openscada.opc.lib.da.browser.Leaf;
+import org.openscada.opc.lib.da.browser.TreeBrowser;
+
+public class OPCInitialTagConfig {
+	private static OPCInitialTagConfig instance = null;
+	public static final String NO_SUBGROUP_MSG = "Unable to find sub-group: %s %nPossible Matches:%s";
+	public static final String ERROR_CODE = "errorCode";
+	public static final String QUALITY = "quality";
+	public static final String TIMESTAMP = "timestamp";
+	public static final String VALUE = "value";
+
+	public static OPCInitialTagConfig getInstance() {
+		if (instance == null) {
+			synchronized (OPCInitialTagConfig.class) {
+				instance = new OPCInitialTagConfig();
+			}
+		}
+		return instance;
+	}
+
+	public synchronized String fetchTagState(Map<String, Item> opcTags, ComponentLog logger) throws JIException {
+		Map<String, Map<String, Object>> data = new TreeMap<String, Map<String, Object>>();
+		TimeZone timeZone = TimeZone.getTimeZone("UTC");
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EE MMM dd HH:mm:ss zzz yyyy", Locale.US);
+		simpleDateFormat.setTimeZone(timeZone);
+
+		// logger.info("*****looking for latest data with
+		// opcTags-group-{}-\n{}",new
+		// Object[]{flowfile.getAttribute("groupName"),opcTags});
+
+		StringBuffer output = new StringBuffer();
+		for (String key : opcTags.keySet()) {
+			Item item = opcTags.get(key);
+			// not serializable directly
+			ItemState is = item.read(false);
+			final Map<String, Object> itemStateAsMap = getItemStateAsMap(is);
+			data.put(key, itemStateAsMap);
+			// 1 file per group not per tag
+			output.append(key + "-" + opcTags.size() + "," + itemStateAsMap.get(VALUE) + ","
+					+ simpleDateFormat.format(itemStateAsMap.get(TIMESTAMP)) + "," + itemStateAsMap.get(QUALITY) + ","
+					+ itemStateAsMap.get(ERROR_CODE) + "\n");
+		}
+		return output.toString();
+	}
+
+	public static Map<String, Object> getItemStateAsMap(ItemState is) throws JIException {
+		Map<String, Object> retVal = new TreeMap<String, Object>();
+		retVal.put(ERROR_CODE, Integer.valueOf(is.getErrorCode()));
+		retVal.put(QUALITY, Short.valueOf(is.getQuality()));
+		retVal.put(TIMESTAMP, is.getTimestamp().getTime());
+		retVal.put(VALUE, JIVariantMarshaller.toJavaType(is.getValue()));
+		// retVal.put("value2", JIVariantMarshaller.dumpValue(is.getValue()));
+
+		return retVal;
+	}
+
+	public synchronized Map<String, Item> fetchSpecificTagsMap(Server server, String groupName,
+			Map<String, Item> opcItems, List<String> specificItemIds, ComponentLog logger) throws Exception {
+		Group opcGroup = null;
+		// Map<String, Item> opcItems = new HashMap<String, Item>();
+		List<String> itemIds = new ArrayList<String>();
+		try {
+
+			if (groupName != null && !groupName.isEmpty()) {
+				try {
+					opcGroup = server.addGroup(groupName);
+				} catch (DuplicateGroupException ex) {
+					// return opcItems;
+					logger.warn("Duplicate Group {}" + groupName);
+					opcGroup = server.findGroup(groupName);
+				}
+			}
+
+			if (specificItemIds != null) {
+				if (opcGroup != null) {
+					for (String specificItemId : specificItemIds) {
+						itemIds.add(specificItemId);
+						try {
+							Item i = opcGroup.addItem(specificItemId);
+							opcItems.put(specificItemId, i);
+						} catch (AddFailedException e) {
+							// may be item is invalid - skip for POC
+							continue;
+						}
+					}
+				}
+
+			}
+
+			return opcItems;
+
+		} catch (Exception ex) {
+			throw new Exception(ex.getMessage(), ex);
+		}
+	}
+
+	public List<String> fetchAllTags(Server server, ComponentLog logger) throws Exception {
+
+		List<String> itemIds = new ArrayList<String>();
+		try {
+			TreeBrowser treeBrowser = server.getTreeBrowser();
+			Branch root = treeBrowser.browse();
+			Branch parent = root;
+
+			ArrayList<String> possibleBranches = new ArrayList<String>();
+			StringBuilder possibleMatches = new StringBuilder();
+			for (Branch b : parent.getBranches()) {
+				possibleBranches.add(String.format("%n[B] %s", b.getName()));
+			}
+			Collections.sort(possibleBranches, String.CASE_INSENSITIVE_ORDER);
+			ArrayList<String> possibleLeaves = new ArrayList<String>();
+			for (Leaf l : parent.getLeaves()) {
+				possibleLeaves.add(String.format("%n[T] %s", l.getName()));
+			}
+			Collections.sort(possibleLeaves, String.CASE_INSENSITIVE_ORDER);
+
+			for (String s : possibleBranches) {
+				possibleMatches.append(s);
+			}
+			for (String s : possibleLeaves) {
+				possibleMatches.append(s);
+			}
+
+			populateItemsMapRecursive(parent, itemIds);
+
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			throw e;
+		}
+
+		return itemIds;
+
+	}
+
+	public void populateItemsMapRecursive(Branch parent, List<String> itemIds) throws JIException, AddFailedException {
+		for (Leaf l : parent.getLeaves()) {
+			registerLeaf(l, itemIds);
+		}
+		for (Branch child : parent.getBranches()) {
+			populateItemsMapRecursive(child, itemIds);
+		}
+	}
+
+	private void registerLeaf(Leaf l, List<String> itemIds) throws JIException, AddFailedException {
+		String itemId = l.getItemId();
+		itemIds.add(itemId);
+		// if (opcGroup != null) {
+		// Item i = opcGroup.addItem(itemId);
+		// }
+	}
+
+	public synchronized void unregisterGroup(Server server, String groupName, ComponentLog logger) throws JIException {
+		Group opcGroup;
+		try {
+			logger.info("Trying to Unregister Group []", new Object[] { groupName });
+			opcGroup = server.findGroup(groupName);
+
+			if (opcGroup != null) {
+				server.removeGroup(opcGroup, true);
+			}
+		} catch (IllegalArgumentException | UnknownHostException | UnknownGroupException | NotConnectedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ nifi-nar-bundles/nifi-opcda-bundle/nifi-opcdaclient-processors/src/test/java/org/apache/nifi/processors/opcdaclient/processors/QueryTagStateTest.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nifi.processors.opcdaclient.processors;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.nifi.util.MockFlowFile;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class QueryTagStateTest {
+
+    private TestRunner testRunner;
+
+    @Before
+    public void init() {
+        testRunner = TestRunners.newTestRunner(QueryTagState.class);
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", java.util.logging.Level.INFO.toString());
+        java.util.logging.Logger.getLogger("org.jinterop").setLevel(java.util.logging.Level.OFF);
+    }
+
+    
+    @Test
+    public void testQueryTagState() throws IOException {
+
+    	List<MockFlowFile> flowFiles  = null;
+        final TestRunner runner = TestRunners.newTestRunner(new QueryTagState());
+
+        runner.setProperty(QueryTagState.OPCDA_SERVER_IP_NAME, "targetopc.l3network.local");
+        runner.setProperty(QueryTagState.OPCDA_WORKGROUP_NAME, "WORKGROUP");
+        runner.setProperty(QueryTagState.OPCDA_USER_NAME, "bamboo");
+        runner.setProperty(QueryTagState.OPCDA_PASSWORD_TEXT, "paper");
+        runner.setProperty(QueryTagState.OPCDA_CLASS_ID_NAME, "B3AF0BF6-4C0C-4804-A1222-6F3B160F4397");
+        runner.setProperty(QueryTagState.READ_TIMEOUT_MS_ATTRIBUTE, "60000");
+        runner.setProperty(QueryTagState.POLL_REPEAT_MS_ATTRIBUTE, "3600000");
+        runner.setProperty(QueryTagState.IS_ASYNC_ATTRIBUTE,"Y");
+        Map<String, String> attributes1 = new HashMap<String,String>();
+        attributes1.put("groupName", "FU-13");
+        //attributes1.put("fragment.index", "1");
+        runner.enqueue("Channel1.Device1.Tag10\n_System._ProjectTitle\n_System._TotalTagCount\n_System._DateTime\n_System._ActiveTagCount\n", attributes1);
+        Map<String, String> attributes2 = new HashMap<String,String>();
+        attributes2.put("groupName", "FU-14");
+        runner.enqueue("Channel1.Device1.Tag1000\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1001\nChannel1.Device1.Tag10001\n",attributes2);
+        Map<String, String> attributes3 = new HashMap<String,String>();
+        attributes3.put("groupName", "FU-15");
+        runner.enqueue("Channel1.Device1.Tag102\nChannel1.Device1.Tag10002\nChannel1.Device1.Tag10003\n",attributes3);
+        Map<String, String> attributes4 = new HashMap<String,String>();
+        attributes4.put("groupName", "FU-16");
+        runner.enqueue("Channel1.Device1.Tag102\nChannel1.Device1.Tag10000\n",attributes4);
+        Map<String, String> attributes5 = new HashMap<String,String>();
+        attributes5.put("groupName", "FU-17");
+        runner.enqueue("Channel1.Device1.Tag103\nChannel1.Device1.Tag10000\n",attributes5);
+        Map<String, String> attributes6 = new HashMap<String,String>();
+        attributes6.put("groupName", "FU-18");
+        runner.enqueue("Channel1.Device1.Tag104\nChannel1.Device1.Tag10000\n",attributes6);
+        Map<String, String> attributes7 = new HashMap<String,String>();
+        attributes7.put("groupName", "FU-19");
+        runner.enqueue("Channel1.Device1.Tag105\nChannel1.Device1.Tag10000\n",attributes7);
+        Map<String, String> attributes8 = new HashMap<String,String>();
+        attributes8.put("groupName", "FU-20");
+        runner.enqueue("Channel1.Device1.Tag106\nChannel1.Device1.Tag10000\n",attributes8);
+        Map<String, String> attributes9 = new HashMap<String,String>();
+        attributes9.put("groupName", "FU-21");
+        runner.enqueue("Channel1.Device1.Tag1001\nChannel1.Device1.Tag10000\n",attributes9);
+        Map<String, String> attributes10 = new HashMap<String,String>();
+        attributes10.put("groupName", "FU-22");
+        runner.enqueue("Channel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1007\nChannel1.Device1.Tag10007\nChannel1.Device1.Tag1008\nChannel1.Device1.Tag10008\nChannel1.Device1.Tag1009\nChannel1.Device1.Tag10009\nChannel1.Device1.Tag1003\nChannel1.Device1.Tag10003\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10003\nChannel1.Device1.Tag1003\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1006\nChannel1.Device1.Tag10006\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\nChannel1.Device1.Tag1002\nChannel1.Device1.Tag10000\n",attributes10);
+        //runner.setProperty(PollOpcUaProcessor.NODE_ID_ATTRIBUTE, "ns=2;s=MyObjectsFolder");
+        //Server ns=0 -> ServerStatus (2256 parent) -> CurrentTime NodeId=2258  
+        //runner.setProperty(PollOpcUaProcessor.NODE_ID_ATTRIBUTE, "ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258,ns=0;i=2259,ns=0;i=2258");
+        
+        runner.setThreadCount(20);
+        runner.run(10,true,true);
+        
+        runner.assertQueueEmpty();
+        flowFiles = runner.getFlowFilesForRelationship(QueryTagState.REL_SUCCESS);
+        //runner.assertAllFlowFilesTransferred(QueryTagState.REL_SUCCESS, 4);
+        runner.assertTransferCount(QueryTagState.REL_SUCCESS, 10);
+        flowFiles.get(0).assertAttributeEquals("path", "target");
+
+    }
+    
+    
+
+
+}
